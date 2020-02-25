94336fa92cf096375a44b8396a9a40544c36c20e
s. galiamov <sgalyamov@gmail.com>
2020-02-25 21:56:47 +0100

    created functional extensions

---
+++ src/Illuminator/Functional/ILEmitterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Illuminator.Functional
+{
+    // todo: generate all methods
+    public static class ILEmitterExtensions
+    {
+        public static ILEmitterAction Or(Action<ILEmitter> a, Action<ILEmitter> b) => new ILEmitterAction((ILEmitter il) => il.Or(a, b));
+
+        public static ILEmitterAction AreSame(Action<ILEmitter> a, Action<ILEmitter> b) => new ILEmitterAction((ILEmitter il) => il.AreSame(a, b));
+
+        public static ILEmitterAction LoadInteger(int value) => new ILEmitterAction((ILEmitter il) => il.LoadInteger(value));
+
+        public static ILEmitterAction LoadArgument(ushort argumentIndex) => new ILEmitterAction((ILEmitter il) => il.LoadArgument(argumentIndex));
+
+        public static ILEmitterAction Call(MethodInfo methodInfo) => new ILEmitterAction((ILEmitter il) => il.Call(methodInfo));
+    }
+}

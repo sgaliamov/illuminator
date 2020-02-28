95dad1da27302ef1ab218e9a786d49041708d123
s. galiamov <sgalyamov@gmail.com>
2020-02-28 08:56:37 +0100

    start generate code

---
+++ src/Illuminator/Functional.generated.cs
@@ -0,0 +1,18 @@
+﻿/*
+   ___ ___ _  _ ___ ___    _ _____ ___ ___     ___ ___  ___  ___
+  / __| __| \| | __| _ \  /_\_   _| __|   \   / __/ _ \|   \| __|
+ | (_ | _|| .` | _||   / / _ \| | | _|| |) | | (_| (_) | |) | _|
+  \___|___|_|\_|___|_|_\/_/ \_\_| |___|___/   \___\___/|___/|___|
+
+*/
+
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Illuminator
+{
+    public static class Functional
+    {
+
+        public static ILEmitterAction DefineLabel(out Label label) => new ILEmitterAction((ILEmitter il) => il.DefineLabel(out Label label));

f97f8c910632d3e9b52b02bf0b07de8550623feb
s. galiamov <sgalyamov@gmail.com>
2020-05-09 22:29:12 +0200

    commented code

--- src/Illuminator/Extensions/TypeExtensions.cs
+++ src/Illuminator/Extensions/TypeExtensions.cs
@@ -14,7 +14,7 @@ namespace Illuminator.Extensions
             type.IsPrimitive
             || type.IsEnum
             || ReferenceEquals(type, typeof(string))
-            || ReferenceEquals(type, typeof(object))
+            // || ReferenceEquals(type, typeof(object)) // todo: 3. move extensions to separate assembly
             || ReferenceEquals(type, typeof(decimal));
 
         public static bool ImplementsGenericInterface(this Type type, Type generic) => type.FindGenericInterface(generic) != null;

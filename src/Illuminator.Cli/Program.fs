17b2597b91102e38f69e2e3d4f64c960e59c7e64
s. galiamov <sgalyamov@gmail.com>
2021-03-13 16:13:03 +0100

    json template generator

--- src/Illuminator.Cli/Program.fs
+++ src/Illuminator.Cli/Program.fs
@@ -3,5 +3,6 @@ let main args =
     match args with
     | [| "emit" |]-> printfn "%s" (EmitGenerator.generate())
     | [| "core" |]-> printfn "%s" (CoreGenerator.generate())
+    | [| "json" |]-> printfn "%s" (JsonGenerator.generate())
     | _ -> ()
     0

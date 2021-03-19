7d84f64e854f9083faa1624d5946e43c228cccfc
s. galiamov <sgalyamov@gmail.com>
2021-03-19 23:12:12 +0100

    code coverage

--- src/Illuminator/Exceptions/IlluminatorException.cs
+++ src/Illuminator/Exceptions/IlluminatorException.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 
 namespace Illuminator.Exceptions
 {
     [Serializable]
+    [ExcludeFromCodeCoverage]
     public class IlluminatorException : Exception
     {
         public IlluminatorException() { }
